b1b0db2b9e574aa0b8b5d1578af7582132a226ed
Dmytro Tereshchenko <buxdmo@gmail.com>
2023-03-12 22:10:05 +0200

    Homework 6: Add character device initialization and exit
    Signed-off-by: Dmytro Tereshchenko <buxdmo@gmail.com>

--- task6-chardev-module/main.c
+++ task6-chardev-module/main.c
@@ -1,28 +1,119 @@
 // SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note
 #include <linux/init.h>
-#include <linux/kernel.h
+#include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/device.h>
+#include <linux/cdev.h>
+#include <linux/fs.h>
+#include <linux/uaccess.h>
 
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("Chardev module");
+MODULE_DESCRIPTION("Chrdev module");
 MODULE_AUTHOR("Tereshchenko Dmytro <buxdmo@gmail.com>");
 MODULE_VERSION("1.0");
 
 #undef pr_fmt
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
+#define CHRDEV_CLASS_NAME "chrdev-module"
+#define CHRDEV_DEVICE_NAME "chrdev-module"
+#define CHRDEV_BUFFER_SIZE 1024
+
+static struct class *chrdev_class;
+static struct device *chrdev_device;
+static struct cdev chrdev_cdev;
+static dev_t chrdev_dev;
+
+static int chrdev_major;
+static int chrdev_is_open;
+
+static int chrdev_buffer_size;
+static unsigned char chrdev_buffer[BUFFER_SIZE];
+
 /*===============================================================================================*/
-static int driver_init(void)
+static int chrdev_open(struct inode *inode, struct file *file)
 {
-	pr_info("module init\n");
-	return 0;
+    // TODO: Implement this function
+    return 0;
 }
 /*===============================================================================================*/
-static void driver_exit(void)
+static int chrdev_release(struct inode *inode, struct file *file)
+{
+    // TODO: Implement this function
+    return 0;
+}
+/*===============================================================================================*/
+static ssize_t chrdev_read(struct file *file, char __user *buf, size_t count, loff_t *pos)
+{
+    // TODO: Implement this function
+    return 0;
+}
+/*===============================================================================================*/
+static ssize_t chrdev_write(struct file *file, const char __user *buf, size_t count, loff_t *pos)
+{
+    // TODO: Implement this function
+    return 0;
+}
+/*===============================================================================================*/
+static struct file_operations chrdev_fops = {
+    .owner = THIS_MODULE,
+    .open = chrdev_open,
+    .release = chrdev_release,
+    .read = chrdev_read,
+    .write = chrdev_write,
+};
+/*===============================================================================================*/
+static int chrdev_module_init(void)
+{
+	pr_info("module init\n");
+
+	chrdev_major = alloc_chrdev_region(&chrdev_dev, 0, 1, CHRDEV_DEVICE_NAME);
+	if (chrdev_major < 0) {
+		pr_err("alloc_chrdev_region failed: %d\n", chrdev_major);
+		return chrdev_major;
+	}
+
+	cdev_init(&chrdev_cdev, &chrdev_fops);
+	if (cdev_add(&chrdev_cdev, chrdev_dev, 1) < 0) {
+		pr_err("cdev_add failed\n");
+		goto cdev_add_err;
+	}
+
+	chrdev_class = class_create(THIS_MODULE, CHRDEV_CLASS_NAME);
+	if (IS_ERR(chrdev_class)) {
+		pr_err("class_create failed\n");
+		goto class_create_err;
+	}
+
+	chrdev_device = device_create(chrdev_class, NULL, chrdev_dev, NULL, CHRDEV_CLASS_NAME"0");
+	if (IS_ERR(chrdev_device)) {
+		pr_err("device_create failed\n");
+		goto device_create_err;
+	}
+
+	pr_info("create device with major = %d, minor = %d\n", MAJOR(chrdev_dev), MINOR(chrdev_dev));
+
+	return 0;
+
+device_create_err:
+	class_destroy(chrdev_class);
+class_create_err:
+	cdev_del(&chrdev_cdev);
+cdev_add_err:
+	unregister_chrdev_region(chrdev_dev, 1);
+
+	return -1;
+}
+/*===============================================================================================*/
+static void chrdev_module_exit(void)
 {
 	pr_info("module exit\n");
+	device_destroy(chrdev_class, chrdev_dev);
+	class_destroy(chrdev_class);
+	cdev_del(&chrdev_cdev);
+	unregister_chrdev_region(chrdev_dev, 1);
 }
 /*===============================================================================================*/
 
-module_init(driver_init);
-module_exit(driver_exit);
+module_init(chrdev_module_init);
+module_exit(chrdev_module_exit);
